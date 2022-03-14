4534fbda27ebd881eb1bea981004f30efdbd3f8b
Vincent Nys <vincent.nys@ap.be>
2022-03-14 22:31:19 +0100

    Werkt nu met routering. Kan beginnen allerlei drill types uit te werken.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes
+} from "react-router-dom";
 import './App.css';
 import RandomChordDrillPage from './Pages/RandomChordDrillPage';
 
 function App() {
-  return <RandomChordDrillPage />
+  return (
+    <>
+      <Router>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/random-chord-drill">Random akkoorden</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/random-chord-drill" element={<RandomChordDrillPage />}>
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
